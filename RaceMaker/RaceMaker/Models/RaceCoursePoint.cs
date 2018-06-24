b7690d13faac2941968b036add7f454fb8632416
tmisaacs19 <tmisaacs19@gmail.com>
2018-06-23 22:03:03 -0500

    Updated CSPROJ file to update file paths for RaceSignUp and RaceCoursePoint Models

---
+++ RaceMaker/RaceMaker/Models/RaceCoursePoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RaceMaker.Models
+{
+    public class RaceCoursePoint
+    {
+        [Key]
+        public int ID { get; set; }
+        public int PointNumber { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        
+        public int CreateRaceID { get; set; }
+        [ForeignKey("CreateRaceID")]
+        public CreateRace CreateRace { get; set; }
+
+    }
+}
---
+++ RaceMaker/RaceMaker/Models/RaceSignUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RaceMaker.Models
+{
+    public class RaceSignUp
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required]
+        [Display(Name = "Date Of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [Display(Name = "T-Shirt Size")]
+        public string TshirtSize { get; set; }
+
+        [Required]
+        [Display(Name = "Participant Email")]
+        public string Email { get; set; }
+
+        public int RaceID { get; set; }
+
+
+        [ForeignKey("RaceSignUpID")]
+        public virtual ICollection<CreateRace> CreateRaces { get; set; } = new HashSet<CreateRace>();
+
+
+
+
+    }
+}
